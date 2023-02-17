ea9104c0bbb63cf9a0b3bed2a35da56ed471adb6
Konixy <konixy.p@gmail.com>
2023-02-17 17:10:39 +0100

    "Update"

--- install.sh
+++ install.sh
@@ -6,10 +6,14 @@ cp ./git-sync.sh ~/bin/
 
 chmod +x ~/bin/git-sync.sh
 
+cp ./git-remove-branch.sh ~/bin/
+
+chmod +x ~/bin/git-remove-branch.sh
+
 export PATH="$PATH:$HOME/bin"
 
-# shellcheck source=~/.bashrc
-. ~/.bashrc
+# # shellcheck source=~/.bashrc
+# . ~/.bashrc
 
-# shellcheck source=~/.zshrc
-. ~/.zshrc
+# # shellcheck source=~/.zshrc
+# . ~/.zshrc
